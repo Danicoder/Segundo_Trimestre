944f7e6fbfe5acfde077f54b13e7c057269d11ee
Danicoder <56139050+Danicoder@users.noreply.github.com>
2021-02-24 15:58:35 +0100

    Pilas con Push y Pop
    explicación de colas y pilas

---
+++ practicasClases/practicasClases/listas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic; //nops sirve para trabajar con pilas
+using System.Text;
+
+namespace practicasClases
+{
+    class listas //más reserva dinámicas de memoria
+    {
+        
+    }
+    /*Stack<string> mi_pila = new Stack<string>;
+          Tipo de dato  nombre_variable =  TD
+
+          para los métodos:
+          pusch() --> coloca en cada fila las variables
+          pop() --> te saca de la pila las variables
+          elementos que abarca la pila: .count
+     */
+}
