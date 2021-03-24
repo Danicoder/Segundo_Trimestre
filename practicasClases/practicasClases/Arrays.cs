1be1b3aad3171ed03dcf619f675c430d1113da2f
Danicoder <56139050+Danicoder@users.noreply.github.com>
2021-03-24 16:00:27 +0100

    Ejercicio array

---
+++ practicasClases/practicasClases/Arrays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practicasClases
+{
+    class Arrays
+    {
+        public static void RecorrerNumbers()
+        {
+            Console.WindowHeigh
+
+        }
+    }
+}
